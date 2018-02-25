2c056323c8f6fe454ec37bb3a50284832e353840
User <User@DESKTOP-CULH8TK>
2018-02-25 14:29:19 +0100

    max file

--- index.php
+++ index.php
@@ -1,5 +1,39 @@
 <?php
 
+// Returns a file size limit in bytes based on the PHP upload_max_filesize
+// and post_max_size
+function file_upload_max_size() {
+  static $max_size = -1;
+
+  if ($max_size < 0) {
+    // Start with post_max_size.
+    $post_max_size = parse_size(ini_get('post_max_size'));
+    if ($post_max_size > 0) {
+      $max_size = $post_max_size;
+    }
+
+    // If upload_max_size is less, then reduce. Except if upload_max_size is
+    // zero, which indicates no limit.
+    $upload_max = parse_size(ini_get('upload_max_filesize'));
+    if ($upload_max > 0 && $upload_max < $max_size) {
+      $max_size = $upload_max;
+    }
+  }
+  return $max_size;
+}
+
+function parse_size($size) {
+  $unit = preg_replace('/[^bkmgtpezy]/i', '', $size); // Remove the non-unit characters from the size.
+  $size = preg_replace('/[^0-9\.]/', '', $size); // Remove the non-numeric characters from the size.
+  if ($unit) {
+    // Find the position of the unit in the ordered string which is the power of magnitude to multiply a kilobyte by.
+    return round($size * pow(1024, stripos('bkmgtpezy', $unit[0])));
+  }
+  else {
+    return round($size);
+  }
+}
+
 function sec_file_upload($f_name, $ext) {
     $sess_id = session_id();
     $target_dir = "tmp_uploads/";
@@ -14,8 +48,8 @@ function sec_file_upload($f_name, $ext) {
         return 1;
     }
     // Check file size up to 50MB
-    if ($_FILES["fileToUpload"]["size"] > 50000000) {
-        echo "Sorry, your file is too large.";
+    if ($_FILES[$f_name]["size"] > file_upload_max_size()) {
+        echo "Sorry, your file is too large(Limit:".file_upload_max_size()."). Contact system administrator.";
         return 2;
     }
     if (move_uploaded_file($_FILES[$f_name]["tmp_name"], $target_file)) {
